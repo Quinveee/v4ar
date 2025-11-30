52259b1839a360937b43d1d450a8b7ef17877060
Oliver van Erven <ovanerven@gmail.com>
2025-11-30 14:00:50 +0100

    clean control (still need to remove line detector stuff)

--- src/control/control_strategies/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_strategy import BaseControlStrategy
-from .go_to_target_strategy import GoToTargetStrategy
-
-__all__ = ['BaseControlStrategy', 'GoToTargetStrategy']
-
--- src/control/curling_control/navigation_strategies/__init__.py
+++ src/control/curling_control/navigation_strategies/__init__.py
@@ -4,8 +4,6 @@ from .base_strategy import BaseNavigationStrategy
 from .potential_field_strategy import PotentialFieldStrategy
 from .direct_goal_strategy import DirectGoalStrategy
 from .dwa_strategy import DWAStrategy
-from .integrated_yaw_direct_goal import IntegratedYawDirectGoalStrategy
-# from .viewpoint_strategy import ViewpointStrategy
 from .discretized_strategy import GridDirectGoalStrategy
 from .discretized_obstacle_strategy import GridDirectGoalStrategyObstacle
 from .discrete_forcefield import GridPotentialFieldStrategy
